00a08efb3c8ab56774d252170b01b9f9853fde36
SammirDev <166850749+SammirDev@users.noreply.github.com>
2024-09-30 00:23:54 -0600

    Cambio al nombre de la carpeta

---
+++ Video1/Form1.h
@@ -0,0 +1,88 @@
+#pragma once
+
+namespace CppCLRWinFormsProject {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ lbl_hw;
+	protected:
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->lbl_hw = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// lbl_hw
+			// 
+			this->lbl_hw->AutoSize = true;
+			this->lbl_hw->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->lbl_hw->Font = (gcnew System::Drawing::Font(L"Mongolian Baiti", 15.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->lbl_hw->Location = System::Drawing::Point(245, 229);
+			this->lbl_hw->Name = L"lbl_hw";
+			this->lbl_hw->Size = System::Drawing::Size(162, 23);
+			this->lbl_hw->TabIndex = 0;
+			this->lbl_hw->Text = L"HOLA MUNDO";
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ActiveBorder;
+			this->ClientSize = System::Drawing::Size(668, 496);
+			this->Controls->Add(this->lbl_hw);
+			this->Name = L"Form1";
+			this->Text = L"Video1";
+			this->Load += gcnew System::EventHandler(this, &Form1::Form1_Load);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void Form1_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
